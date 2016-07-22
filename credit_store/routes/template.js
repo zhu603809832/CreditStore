eae873e9a452858e1b5b1f2a7dd50f0e4c8e0514
zhuzhuowei <zhu603809832@163.com>
2016-07-22 18:24:11 +0800

    delete the routes directory.

--- credit_store/routes/template.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('template', { title: 'Express Template' });
-});
-
-module.exports = router;
