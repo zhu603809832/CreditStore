9a1b67aeaabe0fb6b06260c0098951f12fea7ab0
zhuzhuowei <zhu603809832@163.com>
2016-08-11 11:01:31 +0800

    commit the modify in my working machine.

---
+++ credit_store/middlewares/request_log.js
@@ -0,0 +1,24 @@
+var logger = require('../lib/log');
+
+module.exports = function (req, res, next) {
+  // Assets do not out log.
+  if (exports.ignore.test(req.url)) {
+    next();
+    return;
+  }
+
+  var t = new Date();
+  logger.info('\n\nStarted', t.toISOString(), req.method, req.url, req.ip);
+
+  res.on('finish', function () {
+    var duration = ((new Date()) - t);
+    console.log("test string colors".red);
+    console.log("test string colors".green);
+    console.log("test string colors".blue);
+    logger.info('Completed', res.statusCode, ('(' + duration + 'ms)').green);
+  });
+
+  next();
+};
+
+exports.ignore = /^\/(public|agent)/;
